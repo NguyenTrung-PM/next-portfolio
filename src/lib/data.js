61a7fff7fefa6fb627f6193f74f6a8cd3465ea7f
nguyentrungpm <ntcoder.contact@gmail.com>
2023-11-22 01:23:57 +0700

    Finish setup

---
+++ src/lib/data.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { CgWorkAlt } from 'react-icons/cg'
+import { FaReact } from 'react-icons/fa'
+import { LuGraduationCap } from 'react-icons/lu'
+import corpcommentImg from '~/public/corpcomment.png'
+import rmtdevImg from '~/public/rmtdev.png'
+import wordanalyticsImg from '~/public/wordanalytics.png'
+
+export const links = [
+    {
+        name: 'Home',
+        hash: '#home'
+    },
+    {
+        name: 'About',
+        hash: '#about'
+    },
+    {
+        name: 'Projects',
+        hash: '#projects'
+    },
+    {
+        name: 'Skills',
+        hash: '#skills'
+    },
+    {
+        name: 'Experience',
+        hash: '#experience'
+    },
+    {
+        name: 'Contact',
+        hash: '#contact'
+    }
+]
+
+export const experiencesData = [
+    {
+        title: '',
+        location: '',
+        description: '',
+        icon: React.createElement(),
+        date: ''
+    }
+]
+
+export const projectsData = [
+    {
+        title: '',
+        description: '',
+        tags: [],
+        imageUrl: ''
+    }
+]
+
+export const skillsData = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'TypeScript',
+    'React',
+    'Next.js',
+    'Node.js',
+    'Git',
+    'Tailwind',
+    'Java',
+    'Spring-Boot',
+    'MongoDB',
+    'Redux',
+    'Framer Motion'
+]
