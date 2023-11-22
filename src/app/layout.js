8c0fc7e7d3dafd4b2ffc2bcb921c52b646e5385a
nguyentrungpm <ntcoder.contact@gmail.com>
2023-11-22 20:54:00 +0700

    Add base header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import  Header  from '~/src/components/Header'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -9,12 +11,12 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-    // useCallback
     return (
         <html lang="en">
             <body className={`${inter.className} bg-gray-50 text-gray-950`}>
                 <div className="bg-[hsl(358,76%,94%)] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
                 <div className="bg-[hsl(247,82%,91%)] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+                <Header />
                 {children}
             </body>
         </html>
